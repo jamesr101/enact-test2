6ada25df855450b7bf3856baa7c5f678b20e11c0
James Robins <james.robins@fxdigital.uk>
2020-03-04 17:37:09 +0000

    displaying data validation in TextInput

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -3,6 +3,7 @@ import kind from '@enact/core/kind';
 import css from './TextInput.module.less';
 import Heading from '@enact/moonstone/Heading';
 import Input from '@enact/moonstone/Input';
+import BodyText from '@enact/ui/BodyText/BodyText';
 
 const TextInput = kind({
 	name: 'TextInput',
@@ -20,13 +21,19 @@ const TextInput = kind({
 		}
 	},
 
-	render: function ({heading, value, onChange, name, placeholder, ...props}) {
+	render: function ({heading, value, onChange, name, placeholder, errors, ...props}) {
 		const type = (name === 'password') ? 'password' : '';
 
 		return (
 			<div className={props.className}>
 				<Heading>{heading}</Heading>
-				<Input type={type} value={value} onChange={onChange} placeholder={placeholder} />
+				<Input
+				type={type}
+				value={value}
+				onChange={onChange}
+				placeholder={placeholder}
+				/>
+				<BodyText className={css.error}>{errors}</BodyText>
 			</div>
 		);
 	}

95bf4df9396f4c44398cc17c6fa36c929fc7dceb
James Robins <james.robins@fxdigital.uk>
2020-03-04 10:02:43 +0000

    creating SelectInput component

---
+++ src/components/SelectInput/SelectInput.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import kind from '@enact/core/kind';
+import css from './SelectInput.module.less';
+import Heading from '@enact/moonstone/Heading';
+import Dropdown from '@enact/moonstone/Dropdown';
+
+const SelectInput = kind({
+	name: 'SelectInput',
+
+	styles: {
+		css,
+		className: 'selectInput'
+	},
+
+	handlers: {
+		onSelect: (ev, {name, onSelect}) => {
+			if (onSelect) {
+				onSelect(name, ev);
+			}
+		}
+	},
+
+	render: function ({heading, onSelect, children, ...props}) {
+		return (
+			<div className={props.className}>
+				<Heading>{heading}</Heading>
+				<Dropdown
+					title="Select"
+					onSelect={onSelect}
+					>
+					{children}
+				</Dropdown>
+			</div>
+		);
+	}
+});
+
+export default SelectInput;

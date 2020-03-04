2b533524e15c29191f3772dc8ab5d795fed9d7c1
James Robins <james.robins@fxdigital.uk>
2020-03-04 09:31:25 +0000

    creating TextInput component

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import kind from '@enact/core/kind';
+import css from './TextInput.module.less';
+import Heading from '@enact/moonstone/Heading';
+import Input from '@enact/moonstone/Input';
+
+const TextInput = kind({
+	name: 'TextInput',
+
+	styles: {
+		css,
+		className: 'textInput'
+	},
+
+	handlers: {
+		onChange: (ev, {name, onChange}) => {
+			if (onChange) {
+				onChange(name, ev);
+			}
+		}
+	},
+
+	render: function ({heading, value, onChange, name, placeholder, ...props}) {
+		const type = (name == 'password') ? 'password' : '';
+
+		return (
+			<div className={props.textInput}>
+				<Heading>{heading}</Heading>
+				<Input type={type} value={value} onChange={onChange} placeholder={placeholder} />
+			</div>
+		);
+	}
+});
+
+export default TextInput;

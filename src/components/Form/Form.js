548559c81c0498f1a3b8b5d810333a510608dcd3
James Robins <james.robins@fxdigital.uk>
2020-03-04 17:36:48 +0000

    adding data validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -20,6 +20,13 @@ class Form extends Component {
 				password: '',
 				videoId: '',
 			},
+			validationErrors: {
+				player: '',
+				ui: '',
+				username: '',
+				password: '',
+				videoId: '',
+			},
 			isSubmitting: false,
 			isError: false
 		};
@@ -27,11 +34,64 @@ class Form extends Component {
 		this.handleChange = this.handleChange.bind(this);
 	}
 
+	validateData = () => {
+		const {validationErrors, data} = this.state;
+
+		Object.keys(data).forEach( name => {
+			const value = data[name];
+			switch (name) {
+				case 'player':
+					validationErrors.player =
+						value
+							? ''
+							: 'Please select a Player';
+					break;
+				case 'ui':
+					validationErrors.ui =
+						value
+							? ''
+							: 'Please select a UI';
+					break;
+				case 'videoId':
+					validationErrors.videoId =
+						value
+							? ''
+							: 'Please enter a Video ID';
+					break;
+				case 'username':
+					validationErrors.username =
+						value
+							? ''
+							: 'Please enter a Username';
+					break;
+
+				default:
+					break;
+			}
+
+		})
+
+		this.setState({
+			validationErrors: validationErrors
+		})
+
+		const allValid = Object.keys(validationErrors).every(function(i) {
+			return !validationErrors[i].length
+		});
+
+		return allValid
+	}
+
 	handleChange = (name, value) => {
+
 		this.setState(prevState => ({
 			data: {
 				...prevState.data,
 				[name]: value
+			},
+			validationErrors: {
+				...prevState.validationErrors,
+				[name]: ''
 			}
 		}))
 	}
@@ -41,31 +101,33 @@ class Form extends Component {
 		console.log(this.state.data)
 		const data = this.state.data;
 
-		this.setState({
-			isSubmitting: true,
-			isError: false
-		});
-
-		axios.post(`https://jsonplaceholder.typicode.com/users`, { data })
-		.then(res => {
-			this.setState({isSubmitting: false});
-
-			console.log(res);
-			console.log(res.data);
-		})
-		.catch((err) => {
+		if(this.validateData()) {
 			this.setState({
-				isSubmitting: false,
-				isError: true
-			})
+				isSubmitting: true,
+				isError: false
+			});
 
-			console.log(err)
-		});
+			axios.post(`https://jsonplaceholder.typicode.com/users`, { data })
+			.then(res => {
+				this.setState({isSubmitting: false});
+
+				console.log(res);
+				console.log(res.data);
+			})
+			.catch((err) => {
+				this.setState({
+					isSubmitting: false,
+					isError: true
+				})
+
+				console.log(err)
+			});
+		}
 	}
 
 
 	render () {
-		const {data, isSubmitting, isError} = this.state;
+		const {data, isSubmitting, isError, validationErrors} = this.state;
 
 		return (
 			<form className={styles.form} onSubmit={this.handleFormSubmit}>
@@ -80,20 +142,41 @@ class Form extends Component {
 
 				<Row>
 					<Cell>
-						<SelectInput name='player' heading='Video Player' onSelect={this.handleChange}>
+						<SelectInput name='player' heading='Video Player' onSelect={this.handleChange} errors={validationErrors.player}>
 							{['AVPlay', 'DashJS', 'ExoPlayer', 'hls.js', 'HTML5', 'Shaka', 'WebMaf']}
 						</SelectInput>
 
-						<SelectInput name='ui' heading='UI' onSelect={this.handleChange}>
+						<SelectInput name='ui' heading='UI' onSelect={this.handleChange} errors={validationErrors.ui}>
 							{['DPlay', 'Eurosport', 'Motortrend']}
 						</SelectInput>
 					</Cell>
 					<Cell>
-						<TextInput name='videoId' heading='Video ID' value={data.videoId} onChange={this.handleChange} placeholder="Enter Video ID"/>
+						<TextInput
+						name='videoId'
+						heading='Video ID'
+						value={data.videoId}
+						onChange={this.handleChange}
+						placeholder="Enter Video ID"
+						errors={validationErrors.videoId}
+						/>
 
-						<TextInput name='username' heading='Username' value={data.username} onChange={this.handleChange} placeholder="Enter Username"/>
+						<TextInput
+						name='username'
+						heading='Username'
+						value={data.username}
+						onChange={this.handleChange}
+						placeholder="Enter Username"
+						errors={validationErrors.username}
+						/>
 
-						<TextInput name='password' heading='Password' value={data.password} onChange={this.handleChange} placeholder="Enter Password"/>
+						<TextInput
+						name='password'
+						heading='Password'
+						value={data.password}
+						onChange={this.handleChange}
+						placeholder="Enter Password"
+						errors={validationErrors.password}
+						/>
 					</Cell>
 				</Row>
 

0db89e7cefd5390b5f51c5d7df70a9bbd253964d
James Robins <james.robins@fxdigital.uk>
2020-03-05 13:54:23 +0000

    creating multiselect component

---
+++ src/components/MultiSelectInput/MultiSelectInput.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import kind from '@enact/core/kind';
+import css from './MultiSelectInput.module.less';
+import Heading from '@enact/moonstone/Heading';
+import Dropdown from '@enact/moonstone/Dropdown';
+import BodyText from '@enact/ui/BodyText/BodyText';
+import PropTypes from 'prop-types';
+
+const MultiSelectInput = kind({
+	name: 'MultiSelectInput',
+
+	propTypes: {
+		heading: PropTypes.string,
+		value: PropTypes.array,
+		onSelect: PropTypes.func,
+		children: PropTypes.node,
+		errors: PropTypes.string
+	},
+
+	styles: {
+		css,
+		className: 'multiSelectInput'
+	},
+
+	handlers: {
+		onSelect: (ev, {name, onSelect, value}) => {
+			if (onSelect) {
+				const selected = ev.data;
+				if (value.includes(selected)) {
+					value.splice(value.indexOf(selected), 1)
+				} else {
+					value.push(selected)
+				}
+				onSelect(name, value);
+			}
+		}
+	},
+
+	render: function ({heading, value, onSelect, children, errors, ...props}) {
+		return (
+			<div className={props.className}>
+				<Heading>{heading} (Multi)</Heading>
+				<Dropdown
+					title="Select"
+					onSelect={onSelect}
+					>
+					{children}
+				</Dropdown>
+				<BodyText className={css.selected}>Selected: {value.join(', ')}</BodyText>
+				<BodyText className={css.error}>{errors}</BodyText>
+			</div>
+		);
+	}
+});
+
+export default MultiSelectInput;

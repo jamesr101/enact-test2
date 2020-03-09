2594f5e6eb9dde8003fc2f987efe7c1fa0d5166d
James Robins <james.robins@fxdigital.uk>
2020-03-09 12:07:57 +0000

    turing realm into dropdown with input if other

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -21,7 +21,8 @@ class Form extends Component {
 				username: '',
 				password: '',
 				videoId: '',
-				realm: ''
+				realm: '',
+				realmText: ''
 			},
 			validationErrors: {
 				player: '',
@@ -74,6 +75,12 @@ class Form extends Component {
 							? ''
 							: 'Please select a Realm';
 					break;
+				case 'realmText':
+					validationErrors[name] =
+						value
+							? ''
+							: 'Please enter a Realm or select an option above';
+					break;
 
 				default:
 					break;
@@ -88,7 +95,7 @@ class Form extends Component {
 		return validationErrors
 	}
 
-	isDataValide = () => {
+	isDataValid = () => {
 		const validationErrors = this.validateData()
 
 		const allValid = Object.keys(validationErrors).every(function(i) {
@@ -99,7 +106,6 @@ class Form extends Component {
 	}
 
 	handleChange = (name, value) => {
-
 		this.setState(prevState => ({
 			data: {
 				...prevState.data,
@@ -112,12 +118,24 @@ class Form extends Component {
 		}))
 	}
 
+	formatData = (data) => {
+		const formatedData = Object.assign({}, data);
+
+		if ( formatedData.realm === 'Other') {
+			formatedData.realm = formatedData.realmText;
+		}
+
+		delete formatedData.realmText;
+
+		return formatedData
+	}
+
 	handleFormSubmit = (ev) => {
 		ev.preventDefault();
-		const data = this.state.data;
-		console.log(data)
+		const data = this.formatData(this.state.data);
+		console.log(data);
 
-		if(this.isDataValide()) {
+		if(this.isDataValid()) {
 			this.setState({
 				isSubmitting: true,
 				isErrorResponse: false
@@ -151,6 +169,8 @@ class Form extends Component {
 	render () {
 		const {data, isSubmitting, isErrorResponse, validationErrors} = this.state;
 
+
+
 		return (
 			<form className={styles.form} onSubmit={this.handleFormSubmit}>
 				<Row>
@@ -172,16 +192,31 @@ class Form extends Component {
 							{['DPlay', 'Eurosport', 'Motortrend']}
 						</SelectInput>
 
-						<TextInput
+						<SelectInput
 						name='realm'
 						heading='Realm'
 						value={data.realm}
-						onChange={this.handleChange}
+						onSelect={this.handleChange}
 						placeholder="Enter a Realm"
 						errors={validationErrors.realm}
-
 						onSpotlightRight={this.handleSpotlightEndOfColumn}
-						/>
+						>
+						{['Realm 1', 'Realm 2', 'Realm 3', 'Other']}
+						</SelectInput>
+
+						{
+							(data.realm === 'Other') && <TextInput
+							name='realmText'
+							// heading='Realm'
+							value={data.realmText}
+							onChange={this.handleChange}
+							placeholder="Enter a Realm"
+							errors={validationErrors.realmText}
+
+							onSpotlightRight={this.handleSpotlightEndOfColumn}
+							/>
+						}
+
 					</Cell>
 					<Cell>
 						<TextInput
